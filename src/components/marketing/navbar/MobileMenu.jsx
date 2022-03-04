fe42d657dcba353b10a0abb4e41dee602a5a07cb
dilane3 <komboudilane125@gmail.com>
2022-03-04 21:26:28 +0100

    mobile menu ok

--- src/components/marketing/navbar/MobileMenu.jsx
+++ src/components/marketing/navbar/MobileMenu.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import { BsX } from 'react-icons/bs'
 import style from '../../../css/base.module.css'
+import ImgCircle from '../../elements/imgCircle/ImgCircle'
+import Button from '../../elements/buttons/Button'
+import ALink from '../../elements/a/ALink'
+
+const profileImage = require("../../../medias/photos/gabriel-matula-Qhd1tEZo1ew-unsplash (1).jpg")
+
+const NavItem = ({ text }) => {
+  return (
+    <ALink classe={style.mobileMenuNavItem} link="#">
+      <span className={style.mobileMenuNavItemText}>{ text }</span>
+    </ALink>
+  )
+}
 
 const MobileMenu = ({ show, onHide }) => {
   return (
@@ -9,8 +22,40 @@ const MobileMenu = ({ show, onHide }) => {
 				className={style.navbarIconMenuClose}
 				onClick={onHide}	
 			>
-				<BsX />
+				<BsX color="#fff" />
 			</div>
+
+      <div className={style.mobileMenuHeader}>
+        {
+          !false ? (
+            <>
+              <ImgCircle classe={style.mobileMenuHeaderImage} src={profileImage} />
+
+              <div className={style.mobileMenuHeaderInfo}>
+                <span className={style.mobileMenuHeaderName}>Dilane Kombou</span>
+
+                <Button classe={style.mobileMenuHeaderInfoBtn}>Editer le profil</Button>
+              </div>
+            </>
+          ):(
+            <div className={style.mobildeMenuLoginSection}>
+              <span className={style.mobileMenuLoginText}>Authentifiez vous avant de continuer</span>
+
+              <div className={style.mobileMenuControl}>
+                <Button size="small" classe={style.mobileMenuLoginBtn}>CONNEXION</Button>
+                <Button size="small" theme="red" classe={style.mobileMenuLoginBtn}>INSCRIPTION</Button>
+              </div>
+            </div>
+          )
+        }
+      </div>
+
+      <div className={style.mobildeMenuContent}>
+        <div>
+          <NavItem text="Les repetiteurs" />
+          <NavItem text="Deconnexion" />
+        </div>
+      </div>
     </article>
   )
 }
