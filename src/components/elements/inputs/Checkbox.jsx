a54feadb52be37eedf6918d09eba3b6339a08d52
tchassijordan <tchassijordan@gmail.com>
2022-03-08 17:43:13 +0100

    client sign up complete. But lacking animation.

--- src/components/elements/inputs/Checkbox.jsx
+++ src/components/elements/inputs/Checkbox.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Checkbox = ({ text, htmlFor }) => {
+  return (
+    <div className="block md:inline-block">
+      <input
+        id={htmlFor}
+        type="checkbox"
+        name="neuf"
+        className="mb-3 form-checkbox h-4 w-4 text-red-500 transition duration-150 ease-in-out"
+      />
+      <label htmlFor={htmlFor} className="ml-2 mr-4 text-sm check">
+        {text}
+      </label>
+    </div>
+  );
+};
 
-	return(
-		<div className="block md:inline-block">
-	        <input  id={htmlFor} type="checkbox" name="neuf" className="ml-4 form-checkbox h-4 w-4 text-red-500 transition duration-150 ease-in-out" />
-	        <label htmlFor={htmlFor} className="ml-2 text-sm check">{text}</label>
-	      </div>
-	)
-}
-
-export default Checkbox
+export default Checkbox;
--- src/pages/signupClient/body/BodySignupClient.jsx
+++ src/pages/signupClient/body/BodySignupClient.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import H3 from "../../../components/elements/titles/H3";
 import Input from "../../../components/elements/inputs/Input";
-import { FaChevronRight } from "react-icons/fa";
+import Checkbox from "../../../components/elements/inputs/Checkbox";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 const BodySignupClient = () => {
   let [email, setEmail] = useState("");
@@ -14,30 +15,12 @@ const BodySignupClient = () => {
   let [ville, setVille] = useState("");
   let [profilePicture, setProfilePicture] = useState("");
   let [sexe, setSexe] = useState("");
+  let [anneeAcademique, setAnneeAcademique] = useState("");
+  let [isStep2Active, setIsStep2Active] = useState(false);
 
-  return (
-    <div className="flex flex-col justify-center items-center py-14 px-7 w-11/12 h-full max-w-xl mx-auto font-primary">
-      <H3 color="#00171f" classe="mb-10 text-center">
-        Suivre les étapes pour s'inscrire en tant apprenant qui cherche un
-        repétiteur
-      </H3>
-      <div className="flex justify-between text-white w-full items-center">
-        <p className="rounded-full bg-primary font-bold basis-1/12 px-4 py-2 text-center xs:py-4 xs:px-6">
-          1
-        </p>
-        <p className=" bg-gray-400 h-1 basis-5/6 opacity-10"></p>
-        <p className="rounded-full bg-gray-400 font-bold basis-1/12 text-white opacity-10 px-4 py-2 text-center xs:py-4 xs:px-6">
-          2
-        </p>
-      </div>
-      <div className="flex justify-between m-0 w-full text-gray-400 mt-1 text-xs">
-        <p className="w-2/12 text-left">Informations personnelles</p>
-        <p className="w-2/12 max-w-min text-right">Offre de repétition</p>
-      </div>
-      <form
-        className="flex flex-col shadow-md-x py-5 px-3 mt-10 sm:px-5 sm:py-7 rounded-xl w-full"
-        onSubmit={(e) => e.preventDefault()}
-      >
+  let formStep1 = () => {
+    return (
+      <div className="flex flex-col w-full">
         <Input
           type="text"
           name="nom"
@@ -89,7 +72,7 @@ const BodySignupClient = () => {
         <select
           value={ville}
           onChange={(e) => setVille(e.target.value)}
-          className=" bg-white border-b-2 border-gray2 py-2 md:py-3 text-gray-600 text-xs md:text-sm  focus:outline-none focus:bg-gray2-ligth focus:px-6 focus:text-gray-600 mb-3"
+          className=" bg-white border-b-2 border-gray2 py-2 md:py-3 text-gray-600 text-xs md:text-sm w-full focus:outline-none focus:bg-gray2-ligth focus:px-6 focus:text-gray-600 mb-3"
         >
           <option value="">Ville</option>
           <option value="Yaounde">Yoaunde</option>
@@ -105,7 +88,7 @@ const BodySignupClient = () => {
         />
         <label
           htmlFor="profilePicture"
-          className="bg-white border-b-2 border-gray2 py-2 md:py-3 text-gray-400 text-xs md:text-sm  focus:outline-none focus:bg-gray2-ligth focus:px-6 mb-3"
+          className="bg-white border-b-2 border-gray2 py-2 md:py-3 text-gray-400 text-xs md:text-sm  focus:outline-none focus:bg-gray2-ligth focus:px-6 mb-3 w-full block"
         >
           Entrez votre photo de profile
         </label>
@@ -125,7 +108,7 @@ const BodySignupClient = () => {
             <div className="mr-3">
               <input
                 className="after:mr-2"
-                // style={sexe === color ? { color: "rgb(75 85 99)" } : ""}
+                key={color}
                 type="radio"
                 name="sexe"
                 id={color}
@@ -139,10 +122,153 @@ const BodySignupClient = () => {
             </div>
           ))}
         </div>
-        <button className="px-4 py-1.5 my-2 hover:cursor-pointer hover:no-underline bg-primary text-white text-center rounded-full basis-6/12 self-end sm:px-5 flex items-center">
+        <button
+          className=" 
+          self-end
+          flex 
+          items-center 
+          px-4 py-1.5 
+          mb-2 mt-6 
+          hover:cursor-pointer 
+          hover:no-underline 
+          bg-primary 
+          text-white 
+          text-center 
+          rounded-full 
+          basis-6/12 
+          sm:px-5"
+          onClick={() => setIsStep2Active(!isStep2Active)}
+        >
           <span className="text-sm tracking-wide opacity-90">suivant</span>
           <FaChevronRight className="text-white font-bold ml-2" />
         </button>
+      </div>
+    );
+  };
+
+  const formStep2 = () => (
+    <div
+      className="flex flex-col w-full text-gray-400 font-primary"
+      style={isStep2Active ? { display: "flex" } : { display: "none" }}
+    >
+      <p className="font-bold mt-1 tracking-wide">Filières</p>
+      <p className="text-xs md:text-sm">
+        (selectionnez les filières pour se faire repéter)
+      </p>
+      <div className="flex flex-wrap mt-3">
+        {[
+          "Mathématique",
+          "Physique",
+          "Chimie",
+          "Informatique",
+          "SVT",
+          "Anglais",
+          "Français",
+          "Allemand",
+          "Espagnol",
+          "Arabe",
+          "Philosophie",
+          "Histoire-géographie",
+          "Sport",
+          "Dance",
+          "Entrepreunariat",
+          "Marketing",
+          "Economie",
+          "Politique",
+        ].map((matiere) => (
+          <Checkbox text={matiere} htmlFor={matiere} key={matiere} />
+        ))}
+      </div>
+      <p className="mt-5">
+        <span className="font-bold">Niveau scolaire</span>
+        <span className="text-xs md:text-sm">
+          (Entrez votre niveau Respectant un des exemples)
+        </span>
+      </p>
+      <p className="text-xs md:text-sm mb-3">
+        (Ex: cm1 | première C | troisème | niveau 1 physique)
+      </p>
+      <Input
+        classe="md:text-sm"
+        type="text"
+        placeholder="Entrez le niveau académique"
+        id="anneeAcademique"
+        name="anneeAcademique"
+        value={anneeAcademique}
+        handleChange={setAnneeAcademique}
+      />
+      <p className="font-bold mt-4">
+        Présentation de votre recherche de repétition
+      </p>
+      <p className="mt-1 text-xs md:text-sm">
+        (Dècrivez mieux votre recherche de repétiteur)
+      </p>
+      <textarea
+        className="my-5 w-full border text-xs border-gray-400 tracking-wider block focus:outline-none p-2 focus:text-gray-600 focus:bg-gray2-ligth md:text-sm"
+        rows="8"
+        placeholder="Je suis gaelle kengne, éleève de première C en physique je chereche un repétiteur en physique pour mieux comprendre les notions enseignés en cours afin de passer mon probatoire avec une bonne mention. Je uis trés travailleuse et je progresse rapidement, vous pouvez contez sur moi pour respecter votre professionalisme et effectuer les exercices que vous allez me donner. N'hesitez pas à me contacter pour de detail.."
+      ></textarea>
+      <div className="flex justify-between w-full mt-8 mb-2">
+        <button
+          className="px-4 py-1.5  hover:cursor-pointer bg-primary text-white text-center text-xs md:text-sm rounded-full basis-3/12 self-start sm:px-5 flex items-center"
+          onClick={() => setIsStep2Active(!isStep2Active)}
+        >
+          <FaChevronLeft className="text-white font-bold mr-2" />
+          <span className="tracking-wide opacity-90">Precédent</span>
+        </button>
+        <button className="px-4 py-1.5 hover:cursor-pointer bg-primary text-white text-center text-xs md:text-sm rounded-full basis-3/12 self-end sm:px-5">
+          <span className="tracking-wide opacity-90 mx-auto">Terminer</span>
+        </button>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="flex flex-col justify-center items-center py-14 px-7 w-11/12 h-full max-w-xl mx-auto font-primary">
+      <H3 color="#00171f" classe="mb-10 text-center">
+        {isStep2Active
+          ? "Terminer les étapes pour s'inscrire en tant que apprenant qui cherche un repétiteur"
+          : "Suivre les étapes pour s'inscrire en tant apprenant qui cherche un repétiteur"}
+      </H3>
+      <div className="flex justify-between text-white w-full items-center">
+        <p className="rounded-full bg-primary font-bold basis-1/12 px-4 py-2 text-center xs:py-4 xs:px-6">
+          1
+        </p>
+        <p
+          className=" bg-gray-400 h-1 basis-5/6 opacity-10"
+          style={
+            isStep2Active
+              ? {
+                  opacity: "1",
+                  backgroundColor: "rgb(0 168 232 / var(--tw-bg-opacity)",
+                }
+              : null
+          }
+        ></p>
+        <p
+          className="rounded-full bg-gray-400 font-bold basis-1/12 text-white opacity-10 px-4 py-2 text-center xs:py-4 xs:px-6"
+          style={
+            isStep2Active
+              ? {
+                  opacity: "1",
+                  backgroundColor: "rgb(0 168 232 / var(--tw-bg-opacity)",
+                }
+              : null
+          }
+        >
+          2
+        </p>
+      </div>
+      <div className="flex justify-between m-0 w-full text-gray-400 mt-1 text-xs">
+        <p className="w-2/12 text-left">Informations personnelles</p>
+        <p className="w-2/12 max-w-min text-right">Offre de repétition</p>
+      </div>
+      <form
+        className="shadow-md-x py-5 px-3 mt-10 sm:px-5 sm:py-7 rounded-xl w-full"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        {formStep1}
+        {formStep2}
       </form>
     </div>
   );
